b3b5071b5b18ffc9d83371b4c56b8225e157e147
sabay104 <sadok2412@gmail.com>
2025-08-27 16:35:41 +0200

    Update getComputerChoice to return "Rock", "Paper", or "Scissors"

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ function getComputerChoice(){
 
     let randomNumber = Math.floor(Math.random() * 3);
 
-    return randomNumber;
-
+    if (randomNumber === 0) {
+        return "Rock";
+    } else if (randomNumber === 1) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
 
 }
+
+
 
 console.log(getComputerChoice());
